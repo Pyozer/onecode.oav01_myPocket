daf322eac4b1a59c1b9a7012b6aaac769333a2af
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-11-13 17:24:41 +0100

    Redirect to login if user try to access '/' and not connected

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', function (req, res, next) {
+  if (!req.session.isLogged)
+    res.redirect('/login')
+  else
+    res.render('index', { title: 'Express' })
 })
 
 module.exports = router
